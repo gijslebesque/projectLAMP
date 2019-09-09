589db31e9dea7698150399339f76eba96137d347
gijslebesque <gijslebesque@gmail.com>
2019-09-09 23:13:34 +0200

    off command

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -17,15 +17,16 @@ recognition.onresult = function(event) {
     if (event.results[i].isFinal) {
       if (
         event.results[i][0].transcript == "james' is a dick" ||
-        event.results[i][0].transcript == "light on"
+        event.results[i][0].transcript == "light on" ||
+        event.results[i][0].transcript == "light of" ||
+        event.results[i][0].transcript == "light off"
       ) {
         axios
           .post("/postMessage", {
             message: "Go"
           })
           .then(result => {
-            debugger;
-            document.write("I agree");
+            console.log("Success");
           });
       }
       console.log(`You said : ${event.results[i][0].transcript}`);
